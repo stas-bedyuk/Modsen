dc34a0ecfee4b6ab39c909630f14c702c46625a2
Stanislav <98544012+stas-bedyuk@users.noreply.github.com>
2023-06-10 20:16:30 +0300

    Start of development

---
+++ src/main/java/Analysis.java
@@ -0,0 +1,12 @@
+public class Analysis {
+    public static enum Types{
+        Plus,
+        Minus,
+        LBracket,
+        RBracket,
+        Dollar,
+        Ruble,
+        ToDollars,
+        ToRubles
+    }
+}
---
+++ src/main/java/CurrencyCalculator.java
@@ -0,0 +1,18 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CurrencyCalculator {
+
+    public static void main(String[] args) {
+        System.out.print("Enter: ");
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+
+    }
+
+
+}
