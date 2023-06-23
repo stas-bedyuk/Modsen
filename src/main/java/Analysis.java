ead19eca84b8d1fcb241bce1400198ffaec9db02
Stanislav <98544012+stas-bedyuk@users.noreply.github.com>
2023-06-23 19:52:58 +0300

    after review

--- src/main/java/Analysis.java
+++ src/main/java/Analysis.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,7 +12,7 @@ public class Analysis {
 
     private static StackOfString stack = new StackOfString();
 
-    public static double sum =0;
+    public static double sum = 0;
     public static char func = ' ';
     public static char cur = ' ';
 
@@ -19,30 +21,27 @@ public class Analysis {
         int dol = input.indexOf("$");
         int rub = input.indexOf("р");
         input = input.replace(",", ".");
-         if(dol >-1 && rub >-1) {
+        if (dol > -1 && rub > -1) {
             System.out.println("Неверный ввод! (сложение или вычитание разных валют)");
             return -1;
-        }
-        else if(dol>-1) {
-                cur ='$';
+        } else if (dol > -1) {
+            cur = '$';
             pattern = Pattern.compile("\\$(\\d+(?:\\.\\d+)?)|([-+])\\s\\$(\\d+(?:\\.\\d+)?)");
+        } else if (rub > -1) {
+            cur = 'р';
+            pattern = Pattern.compile("(?:\\s*([-+])\\s*)?(\\d+(?:\\.\\d+)?)(р)");
         }
-         else if(rub>-1){
-                 cur ='р';
-             pattern = Pattern.compile("(?:\\s*([-+])\\s*)?(\\d+(?:\\.\\d+)?)(р)");
-         }
-        String match = "";
-        String operator = "";
-        String match2 = "";
+        String match;
+        String operator;
+        String match2;
         Matcher matcher = pattern.matcher(input);
         double total = 0.0;
         while (matcher.find()) {
-            if(rub>-1) {
-                 match = matcher.group(2);
-                 operator = matcher.group(1);
-                 match2 = matcher.group(3);
-            }
-            else{
+            if (rub > -1) {
+                match = matcher.group(2);
+                operator = matcher.group(1);
+                match2 = matcher.group(3);
+            } else {
                 match = matcher.group(1);
                 operator = matcher.group(2);
                 match2 = matcher.group(3);
@@ -62,9 +61,9 @@ public class Analysis {
             }
         }
         return total;
-        }
+    }
 
-    public String simplifyString(String input){
+    public String simplifyString(String input) {
 
         input = " + " + input;
         int start = input.indexOf("toDollars(");
@@ -73,92 +72,93 @@ public class Analysis {
         int count2 = 0;
         int count3 = 0;
         int count4 = 0;
-        while ((start != -1) || (start1 != -1)){
+        while ((start != -1) || (start1 != -1)) {
 
-        if(start1< start && start1 != -1) {
-            start = start1;
-            count1++;
-        }
-        else if(start == -1) {
-            start = start1;
-            count1++;
-        }
-        if (start != -1) {
-            int openBrackets = 0;
-            int end = -1;
-            count2++;
-            for (int i = start + 10; i < input.length(); i++) {
-                char c = input.charAt(i);
+            if (start1 < start && start1 != -1) {
+                start = start1;
+                count1++;
+            } else if (start == -1) {
+                start = start1;
+                count1++;
+            }
+            if (start != -1) {
+                int openBrackets = 0;
+                int end = -1;
+                count2++;
+                for (int i = start + 10; i < input.length(); i++) {
+                    char c = input.charAt(i);
 
-                if (c == '(') {
-                    openBrackets++;
-                } else if (c == ')') {
-                    if (openBrackets == 0) {
-                        end = i;
-                        break;
-                    } else {
-                        openBrackets--;
+                    if (c == '(') {
+                        openBrackets++;
+                    } else if (c == ')') {
+                        if (openBrackets == 0) {
+                            end = i;
+                            break;
+                        } else {
+                            openBrackets--;
+                        }
                     }
                 }
-            }
 
-            if (end != -1) {
+                if (end != -1) {
 
-                String substring = input.substring(start -3, end +1 );
-                if(cur == ' ' && substring.charAt(5) =='D') cur = '$';
+                    String substring = input.substring(start - 3, end + 1);
+                    if (cur == ' ' && substring.charAt(5) == 'D') cur = '$';
 
-                else if(cur == ' ' && substring.charAt(5) =='R') cur = 'р';
-                stack.push(substring);
-                input = input.substring(0, start - 3) + input.substring(end +1);
-                start = input.indexOf("toDollars(");
-                start1 = input.indexOf("toRubles(");
+                    else if (cur == ' ' && substring.charAt(5) == 'R') cur = 'р';
+                    stack.push(substring);
+                    input = input.substring(0, start - 3) + input.substring(end + 1);
+                    start = input.indexOf("toDollars(");
+                    start1 = input.indexOf("toRubles(");
+                } else {
+                    System.out.println("Неправильный формат строки.");
+                }
             } else {
-                System.out.println("Неправильный формат строки.");
+                System.out.println("Подстрока не найдена.");
             }
-        } else {
-            System.out.println("Подстрока не найдена.");
         }
-        }
-        count4 =input.indexOf('$');
+        count4 = input.indexOf('$');
         count3 = input.indexOf('р');
-        if ((count3 != -1 && count4 != -1) ||(count1 >0 && count2 >0) || (count1 >0 && count4 != -1) || (count2 >0 && count3 != -1)) return null;
+        if ((count3 != -1 && count4 != -1) || (count1 > 0 && count2 > 0) || (count1 > 0 && count4 != -1) || (count2 > 0 && count3 != -1))
+            return null;
         return input;
     }
-  public double calculateStringsInStack(){
+
+    public double calculateStringsInStack() {
 
         double amount = 0;
 
         while (!stack.isEmpty()) {
             String str = stack.pop();
             int start = findInnerFunction(str);
-            while(start != -1) {
+            while (start != -1) {
                 int end = str.indexOf(')');   //получили индексы самой внутренней функции
 
 
                 String substring = "";
-                if(func == 'd') substring = str.substring(start + 10, end );
-                else if(func == 'r') substring = str.substring(start + 9, end );
+                if (func == 'd') substring = str.substring(start + 10, end);
+                else if (func == 'r') substring = str.substring(start + 9, end);
 
                 substring = " + " + substring;
-                 substring = substring.replace(",", ".");
+                substring = substring.replace(",", ".");
                 amount = calculate(substring);
                 if (func == 'd') {
-                    if(cur =='$') return -1;
+                    if (cur == '$') return -1;
                     String newstr = toDollars(amount);
-                    str = str.substring(0, start ) + newstr + str.substring(end + 1);
+                    str = str.substring(0, start) + newstr + str.substring(end + 1);
                 } else if (func == 'r') {
-                    if(cur =='р') return -1;
+                    if (cur == 'р') return -1;
                     String newstr = toRubles(amount);
-                    str = str.substring(0, start )  + newstr + str.substring(end + 1);
+                    str = str.substring(0, start) + newstr + str.substring(end + 1);
                 }
                 start = findInnerFunction(str);
             }
-            sum+=calculate(str);
-            amount=0;
+            sum += calculate(str);
+            amount = 0;
         }
 
         return sum;
-  }
+    }
 
     public static int findInnerFunction(String input) {
         int toRublesIndex = input.lastIndexOf("toRubles(");
@@ -167,46 +167,48 @@ public class Analysis {
         if (toRublesIndex == -1 && toDollarsIndex == -1) {
             return -1; // В выражении нет функций toRubles() и toDollars()
         } else if (toRublesIndex == -1) {
-            func ='d';
+            func = 'd';
             return toDollarsIndex; // В выражении нет функции toRubles(), но есть toDollars()
         } else if (toDollarsIndex == -1) {
-            func ='r';
+            func = 'r';
             return toRublesIndex; // В выражении нет функции toDollars(), но есть toRubles()
         } else {
-            if(toRublesIndex > toDollarsIndex) {
-                func ='r';
+            if (toRublesIndex > toDollarsIndex) {
+                func = 'r';
                 return toRublesIndex;
-            }
-
-            else{
-                func ='d';
+            } else {
+                func = 'd';
                 return toDollarsIndex;
             }
         }
     }
-    public String toDollars(double amount){
 
-        return  "$" + (amount/getRate());
-    }
-    public String toRubles(double amount){
+    public String toDollars(double amount) {
 
-        return   (amount*getRate()) +"р";
+        return "$" + (amount / getRate());
     }
 
-    public double getRate(){
-        File file = new File("src/main/resources/rate.txt");
-        double rate =1;
-        try {
-            Scanner scanner = new Scanner(file);
+    public String toRubles(double amount) {
+
+        return (amount * getRate()) + "р";
+    }
+
+    public double getRate() {
+        double rate = 1;
+
+        InputStream inputStream = getClass().getResourceAsStream("/rate.txt");
+        if (inputStream != null) {
+            Scanner scanner = new Scanner(inputStream);
             if (scanner.hasNextDouble()) {
                 rate = scanner.nextDouble();
             } else {
                 System.out.println("В файле не найдено число.");
             }
             scanner.close();
-        } catch (FileNotFoundException e) {
+        } else {
             System.out.println("Файл не найден.");
         }
+
         return rate;
     }
 }
